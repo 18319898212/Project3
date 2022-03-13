12aa3bd0124cb501f0248d7c2a67a581ee76dbf6
18319898212 <90075295+18319898212@users.noreply.github.com>
2022-03-13 17:03:07 +0800

    Update 源.cpp

--- Project3/源.cpp
+++ Project3/源.cpp
@@ -1,29 +1,9 @@
-
-#include<iostream>
-using namespace std;
+#include <iostream>
 int main()
 {
-	int n, i, sum = 0, number = 0, zhengshu = 0;
-	double ave;
-	cin >> n;
-	int* p = new int[n];
-	for (i = 0; i < n; i++)
-	{
-		cin >> p[i];
-		if (p[i] < 0)
-			number++;
-		else if (p[i] > 0)
-		{
-			zhengshu++;
-			sum += p[i];
-		}
-		else
-			continue;
-	}
-	ave = double(sum) / double(zhengshu);
-	cout << "ave=" << ave << " number=" << number << endl;
-	delete[]p;
+	using namespace std;
+	cout << "hello, world";
 	return 0;
-}
-//我是你爹
+	
 
+}
